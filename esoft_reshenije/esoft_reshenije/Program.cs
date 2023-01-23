25b0a4d5264d5bf52f948e3bbb8e44f655e02d99
Tcerber <98079872+Tcerber@users.noreply.github.com>
2023-01-23 13:56:50 +0300

    Add files via upload
    Optimizacija programmy

---
+++ esoft_reshenije/esoft_reshenije/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApplication
+{
+    class OurClass
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2;
+            Console.Write("Введите первое и второе число:\n");
+            try { num1 = float.Parse(Console.ReadLine()); num2 = float.Parse(Console.ReadLine());}
+            catch
+            {
+                Console.WriteLine("Неправильный формат числа!\nОдно из чисел введено неправильно");
+                return;
+            }
+            if (num2 == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя!");
+            }
+            Console.WriteLine("\n" + num1 + " + " + num2 + " = " + (num1 + num2)
+                + "\n" + num1 + " * " + num2 + " = " + (num1 * num2) +
+            "\n" + num1 + " - " + num2 + " = " + (num1 - num2) + "\n" + num1 + " / " + num2 + " = " + (num1 / num2));
+            Console.Write("\nДля выхода из программы нажмите [Enter]:");
+            Console.ReadLine();
+        }
+    }
+}
